574ba202de42c69f15b5802f037e2205e6f0435b
Anthonykyut <pako79096@gmail.com>
2025-03-13 10:14:44 +0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Market from './pages/Market';
 import Landingpage from './components/Landingpage';
 
@@ -7,11 +7,12 @@ import Landingpage from './components/Landingpage';
 function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<Landingpage/>} />
 
-      <Route path="/market" element={< Market/>} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Landingpage/>} />
+        <Route path="/market" element={< Market/>} />
+      </Routes>
+ 
   )
 }
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import "./NavBar"
 
 
@@ -24,10 +25,10 @@ const NavBar = () => {
             <ul className="flex gap-4 justify-center items-center">
                 <li>Sign In</li>
                 <li> 
-                    <a href="/market" className="bg-violet-700 rounded-3xl text-white flex justify-center items-center px-4 py-2 gap-1"> 
+                    <Link to="/market"> <span className="bg-violet-700 rounded-3xl text-white flex justify-center items-center px-4 py-2 gap-1"> 
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-shopping-bag"><path d="M6 2 3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4Z"/><path d="M3 6h18"/><path d="M16 10a4 4 0 0 1-8 0"/></svg>
                         Market
-                    </a>
+                     </span> </Link>
                 </li>
             </ul>
         </div>
